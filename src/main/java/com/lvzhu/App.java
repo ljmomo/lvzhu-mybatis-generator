e703b37e0fed465c48a32640557dd50dffacab1b
lvzhu <lvzhu@youzan.com>
2019-07-22 16:03:44 +0800

    Initial commit

---
+++ src/main/java/com/lvzhu/App.java
@@ -0,0 +1,13 @@
+package com.lvzhu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
